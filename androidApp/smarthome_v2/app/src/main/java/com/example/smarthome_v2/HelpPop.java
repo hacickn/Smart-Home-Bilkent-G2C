74c13dd5d2957771984e9ff441703d4efc1cd921
mutluerengazi <erengazi.mutlu@ug.bilkent.edu.tr>
2020-05-15 00:47:40 +0300

    helper added

---
+++ androidApp/smarthome_v2/app/src/main/java/com/example/smarthome_v2/HelpPop.java
@@ -0,0 +1,24 @@
+package com.example.smarthome_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class HelpPop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_pop);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*.8),(int)(height*.8));
+
+    }
+}
